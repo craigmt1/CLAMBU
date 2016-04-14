7f025f289154bf83d61c9d6524ce6543fe67ead6
craig <craig@unity.(none)>
2016-04-13 23:50:47 -0400

    added to clambu

--- clambu.py
+++ clambu.py
@@ -7,7 +7,6 @@ b64e = lambda s: base64.b64encode(s)
 b64d = lambda s: base64.b64decode(s)
 
 obj = AES.new('sixteen byte key')
-#obj = pyaes.AESModeOfOperationECB('sixteen byte key')
 
 strhex = lambda s: " ".join("{:02x}".format(ord(c)) for c in s)
 
@@ -17,19 +16,6 @@ rev = lambda s: ''.join(reversed(s))
 
 def blocks(m, x=16):
     for i in range(len(m) / x): yield m[i*x:(i+1)*x]
-
-def blocks_inv(c, x=16):
-    for i in range(len(c) / x - 1, -1, -1): yield c[i*x:(i+1)*x]
-
-#@profile
-def cbc(m, obj, iv='\x00'*16):
-    m = pad(m)
-    out = ''
-    for block in blocks(m):
-        iv = strxor(block, iv)
-        iv = obj.encrypt(iv)
-        out += iv
-    return b64e(out)
 
 #@profile
 def jambu(m, obj, Y='\x00'*8, X='\x00'*8):
@@ -72,42 +58,32 @@ def jambu_inv(c, obj, Y='\x00'*8, X='\x00'*8):
     out = ''.join([r for r in jamburound_inv(c, obj, X, Y)])
     return out
 
-def clambu(m, obj, iv='\x00'*16):
-    #round generator
-    def clamburound(m, obj, iv='\x00'*16):
-        X, Y = split(obj.encrypt(iv), 8)
-        R = X
-        for block in blocks(m, 16):
-            P1, P2 = split(block, 8)
-            print strhex(block), " -> ",
-            Y, X = split(obj.encrypt(X+Y), 8)
-            Y = strxor(Y, P1)
-            R = strxor(R, Y)
-            print strhex(Y + strxor(X, P2))
-            print "\tX: %s\tY: %s" % (strhex(X), strhex(Y))
-            yield Y + strxor(X, P2)
+def clamburound(m, obj, iv='\x00'*16):
+    X, Y = split(obj.encrypt(iv), 8)
+    print (strhex(X), " | ", strhex(Y))
+    R = X
+    for block in blocks(m, 16):
+        print strhex(block), " -> ",
+        P1, P2 = split(block, 8)
+        X, Y = split(obj.encrypt(X+Y), 8)
+        R = strxor(P1, X)
+        C1 = strxor(P1, X)
+        C2 = strxor(P2, Y)
+        X, Y = Y, X
+        print strhex(C1 + C2)
+        print "\tX: %s\tY: %s" % (strhex(X), strhex(Y))
+        yield C1 + C2
 
+def clambu_enc(m, obj, iv='\x00'*16):
+    #round generator
     m = pad(m)
     out = ''.join([r for r in clamburound(m, obj, iv)])
     return b64e(out)
 
-def clambu_inv(m, obj, iv='\x00'*16):
+def clambu_dec(m, obj, iv='\x00'*16):
     #round generator
-    def clamburound_inv(m, obj, iv='\x00'*16):
-        X, Y = split(obj.encrypt(iv), 8)
-        R = X
-        for block in blocks(m, 16):
-            C1, C2 = split(block, 8)
-            print strhex(block), " -> ",
-            Y, X = split(obj.encrypt(X+Y), 8)
-            Y = strxor(C1, Y)
-            R = strxor(R, Y)
-            print strhex(Y + strxor(X, C2))
-            print "\tX: %s\tY: %s" % (strhex(X), strhex(Y))
-            yield Y + strxor(X, C2)
-
     m = b64d(m)
-    out = ''.join([r for r in clamburound_inv(m, obj, iv)])
+    out = ''.join([r for r in clamburound(m, obj, iv)])
     return out
 
 #compute execution time of function with supplied arguments over number of iterations
@@ -121,17 +97,14 @@ def bmark(func, args, iterations=100):
 def main():
     m = os.urandom(1000)
     m = pad(m)
-    
-    bmark(cbc, (m,obj,))
+
     bmark(jambu, (m,obj,), 1)
 #main()
 
-#c = jambu("this is a bigger test", obj)
-#print c
-#print jambu_inv(c, obj)
-
-c = clambu("this is a bigger test", obj)
+s = "this is a bigger test of stuff you can do with clambu"
+print s
+c = clambu_enc(s, obj)
 print c
-print clambu_inv(c, obj)
+print clambu_dec(c, obj)
 
 #openssl aes-128-cbc -e -nosalt -K `xxd -p <<< "sixteen byte key"` -iv 0 -in <( echo -n "print \"CBC-MAC not a hash,\"#####################") | xxd -p | tr -d \\n
